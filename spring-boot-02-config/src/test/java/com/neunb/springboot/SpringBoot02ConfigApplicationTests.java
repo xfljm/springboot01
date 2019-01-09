bf9b638f10b4ef578988d6439eaf1b6872e59471
xfljm <wxfsmiple@163.com>
2019-01-09 16:59:40 +0800

    Add files via upload

---
+++ spring-boot-02-config/src/test/java/com/neunb/springboot/SpringBoot02ConfigApplicationTests.java
@@ -0,0 +1,27 @@
+package com.neunb.springboot;
+
+import com.neunb.springboot.bean.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * SpringBoot单元测试：
+ *      可以在测试期间很方便的类似编码一样进行自动注入
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot02ConfigApplicationTests {
+
+    @Autowired
+    private Person person;
+
+    @Test
+    public void contextLoads() {
+        System.out.println(person);
+    }
+
+}
+
